678a8349d7a1fcddb0c4830a353851eb8eebddc1
VidushanMaduranga <vidushanmaduranga0@gmail.com>
2021-09-26 19:12:31 +0530

    Hospital commits

--- app/src/main/java/com/example/blooddonationsystem/availabilityList_source.java
+++
@@ -1,6 +0,0 @@
-package com.example.blooddonationsystem;
-
-public class availabilityList_source {
-
-
-}
--- app/src/main/java/com/example/blooddonationsystem/updateavailability_source.java
+++
@@ -1,4 +0,0 @@
-package com.example.blooddonationsystem;
-
-public class updateavailability_source {
-}
